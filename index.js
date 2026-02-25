329b0cb94d7e1c1d9a9dcaefe07404fedeed660c
Jimena Cardozo <jimena.cardozo@xmartlabs.com>
2026-02-25 12:19:01 -0300

    feat: replace static hero content with real time trending movie data using tmdb trending api, added all details left

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 import { CONFIG } from "./config.js";
 
-async function fetchData(params) {
+async function fetchHeroMovie(params) {
     try {
-        const res = await fetch ('https://api.themoviedb.org/3/movie/11', {
+        const resTrending = await fetch ('https://api.themoviedb.org/3/trending/movie/day', {
             method: 'GET',
             headers: {
                 'accept': 'application/json',
                 'Authorization': `Bearer ${CONFIG.API_KEY}`,
             }
         });
-        if (!res.ok) throw new Error('Error fetching movies');
-        let heroMovie = await res.json();
+        if (!resTrending.ok) throw new Error('Error fetching movies');
+        let data = await resTrending.json();
+        const heroMovie = data.results[0];
+        const resDetail = await fetch (`https://api.themoviedb.org/3/movie/${heroMovie.id}`, {
+            headers: {
+                'Authorization': `Bearer ${CONFIG.API_KEY}`,
+                'accept': 'application/json'
+        }
+        });
+        const movieDetails = await resDetail.json();
+        console.log(movieDetails);
+        const rating = movieDetails.vote_average;     
+        const releaseYear = movieDetails.release_date.slice(0,4);
+        const hours = Math.floor(movieDetails.runtime/60);  
+        const mins = movieDetails.runtime%60;
+        const genres = movieDetails.genres.map(genre =>`<span class="genre">${genre.name}</span>`).join(' ');
         const heroSection = document.querySelector('#hero');
         const imgUrl = `https://image.tmdb.org/t/p/w500${heroMovie.poster_path}`;
         heroSection.innerHTML = `
         <div class="hero-content">
-            <img class="hero-image" src="${imgUrl}" alt="${heroMovie.name}>
+            <img class="hero-image" src="${imgUrl}" alt="${heroMovie.name}">
             <div class="hero-info">
+                <span class="trending-tag">#1 Trending</span>
                 <h1>${heroMovie.title}</h1>
+                <div class="details">
+                    <span class="rating">★ ${rating}</span>
+                    <span class="year">${releaseYear}</span>
+                    <span class="duration"> ◴ ${hours}h ${mins}m</span>
+                    <span class="genres">${genres}</span>
+                </div>
+                <p class="hero-description">${heroMovie.overview}</p>
             </div>
         </div>
         `
@@ -28,5 +50,5 @@ async function fetchData(params) {
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM is ready, requesting data...");
-    fetchData(); 
+    fetchHeroMovie(); 
 });
