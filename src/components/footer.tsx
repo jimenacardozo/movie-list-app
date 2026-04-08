4438d996c7f4bd81869bde6b05b3629d0bcd7a53
Javier Olveyra <jolveyra@gmail.com>
2026-04-08 11:23:08 -0300

    feat: added footer UI

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,13 @@
+import greyfavicon from '../img/greyfavicon.png';
+
 export default function Footer() {
     return <footer>
-        <h1>Footer component</h1>
+        <a className="home footer-title" href="#">
+            <img className="logo" src={greyfavicon} alt="" />
+            CineVault
+        </a>
+        <span>All movie data is fictional and for demonstration purposes
+            only.
+        </span>
     </footer>
 }
