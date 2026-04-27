413fb1954f26a1d14785272d588201382f9db7e7
Jimena Cardozo <jimena.cardozo@xmartlabs.com>
2026-04-27 13:19:00 -0300

    feat: update Footer component styles and add skills-lock.json for Tailwind CSS patterns

--- react-app/src/components/Footer.tsx
+++ react-app/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import './../styles.css';
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className="text-[#7d7d7f] flex flex-col items-center justify-between">
             <a className="home footer-title flex items-center" href="#">
                 <img className="logo w-7.5 h-7.5 m-3" src={greyFaviconUrl} alt="CineVault Logo" />
                 CineVault
